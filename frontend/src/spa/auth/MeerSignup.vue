7b225ce8baa248a1c9e473f1c26a3f5e294edfbd
nerdrun <nerdrun.kai@gmail.com>
2018-12-13 15:10:02 +0900

    #11 회원가입 유효성 검사 리팩토링 중 뻑남

--- frontend/src/spa/auth/MeerSignup.vue
+++ frontend/src/spa/auth/MeerSignup.vue
@@ -7,15 +7,16 @@
       <div class="auth-input-w signup-input-w">
         <div class="auth-input-c">
           <font-awesome-icon icon="user-alt"/>
-          <input type="text" v-model="id" @blur="checkId" placeholder="Your id">
+          <input type="text" v-model="id" @keyup="checkId" placeholder="Your id">
+          <span>{{validationId}}</span>
         </div>
         <div class="auth-input-c">
           <font-awesome-icon icon="key"/>
-          <input type="password" v-model="password" @blur="checkPw" placeholder="Password">
+          <input type="password" v-model="password" @keyup="checkPw" placeholder="Password">
         </div>
         <div class="auth-input-c">
           <font-awesome-icon icon="key" class="re-password"/>
-          <input type="password" v-model="repassword" placeholder="Repeat your password">
+          <input type="password" v-model="repassword" @keyup="checkPw" placeholder="Repeat your password">
         </div>
         <div class="signup-policy-c">
           <input type="checkbox">
@@ -43,11 +44,10 @@ export default {
   name: 'meer-signup',
   data() {
     return {
-      email: '',
       id: '',
-      name: '',
       password: '',
       repassword: '',
+      validationId: ''
     }
   },
   methods : {
@@ -55,27 +55,31 @@ export default {
       this.checkId()
       this.checkPw()
     },
+    validateOfIdLength() {
+      const length = this.id.length
+      if(length < 5) return true
+      else if(length > 30) return true
+      // return (length < 5 || length > 30)
+    },
+    validationRegExp(item, reg) {
+      const regExp = new RegExp(reg, 'g');
+      return regExp.test(item)
+    },
     checkId() {
-      if(this.id.length < 5) {
-        this.id = ''
-        alert('아이디를 5자 이상 입력해주세요')
+      if(this.validateOfIdLength()) {
+        console.log(this.validateOfIdLength())
+        this.validationId = '아이디는 5자 ~ 30자 내외로 입력하세요'
       }
-      if(this.id.length > 30) {
-        this.id = ''
-        alert('아이디를 30자 이하로 입력해주세요.')
-      }
-      else {
-        this.id = this.id.replace(/[^A-Za-z0-9_]/g,'')
-      }
+      if(this.validationRegExp(this.id, '[^\\w]')) this.validationId = '특수 문자 넣지마 시바'
     },
     checkPw() {
       if(this.password.length < 8) {
         this.password = ''
-        alert('암호를 8자 이상 입력해주세요.')
+        // alert('암호를 8자 이상 입력해주세요.')
       } else{
         if(this.password != this.repassword) {
           this.repassword = ''
-          alert('입력하신 비밀번호가 일치하지 않습니다.')
+          // alert('입력하신 비밀번호가 일치하지 않습니다.')
         }
       }
     }
