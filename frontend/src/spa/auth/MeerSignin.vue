33ebfa5ff5167167cc55c722f6fdac41f1592557
ADAMS <nerdrun.adams@gmail.com>
2018-12-14 15:30:00 +0900

    fixed #13 로그인 입력란 유효성 검사 완료

--- frontend/src/spa/auth/MeerSignin.vue
+++ frontend/src/spa/auth/MeerSignin.vue
@@ -17,12 +17,14 @@
       <div class="auth-input-w">
         <div class="auth-input-c">
           <font-awesome-icon icon="user-alt"/>
-          <input type="text" v-model="id" placeholder="Your id">
+          <input type="text" v-model="id" @keyup="checkId" placeholder="Your id">
         </div>
         <div class="auth-input-c">
           <font-awesome-icon icon="key"/>
-          <input type="password" v-model="password" placeholder="Password">
+          <input type="password" v-model="password" @keyup="checkPw" placeholder="Password">
         </div>
+        <p v-if="validationId">{{validationId}}</p>
+        <p v-if="validationPw">{{validationPw}}</p>
         <div class="register-button-w">
           <button>Login</button>
         </div>
@@ -31,14 +33,37 @@
   </div>
 </template>
 <script>
+import validation from '@/commons/validations/auth.validations.js'
 export default {
   name: 'meer-signin',
   data() {
     return {
       id: '',
       password: '',
+      validationId: '',
+      validationPw: ''
     }
-  }
+  },
+  methods : {
+    checkId() {
+      let id = this.id
+      if(validation.length(id, 5, 30)) {
+        this.validationId = '아이디는 5자 ~ 30자 내외로 입력하세요'
+      }
+      else this.validationId = '' 
+      if(validation.regExp(this.id, '[^\\w]')) this.validationId = '특수 문자는 입력하실 수 없습니다.'
+    },
+    checkPw() {
+      let pw = this.password
+      if(validation.length(pw, 8)) {
+        this.validationPw = '암호는 8자 이상 입력하세요'
+      }
+      else this.validationPw = ''
+    },
+    onSubmit() {
+
+    }
+  },
 }
 </script>
 <style lang="scss" src="@/assets/scss/auth/meer-auth.scss"></style>
